9c5a46dc8a4bc26605070ae58b2ffbb1afe624e8
nmauskar <nihar.mauskar@gmail.com>
2017-04-23 00:41:03 -0700

    Initial commit

---
+++ app/src/main/java/ucsd_scholars/tritonfoodpantry/MainActivity.java
@@ -0,0 +1,17 @@
+package ucsd_scholars.tritonfoodpantry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseWrapper db = new firebaseWrapper();
+        db.addMessage("Database!");
+        db.readMsg();
+
+    }
+}
