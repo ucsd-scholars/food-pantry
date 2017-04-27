49c7c7bef871c5e2aae0fe0d4c41d8118332d29c
ddl012 <ddl012@ucsd.edu>
2017-04-26 20:01:16 -0700

    added simple fragment classes

--- app/src/main/java/ucsd_scholars/tritonfoodpantry/HomeActivity.java
+++ app/src/main/java/ucsd_scholars/tritonfoodpantry/HomeActivity.java
@@ -1,28 +1,36 @@
 package ucsd_scholars.tritonfoodpantry;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener,
+                                                    HomeFragment.OnFragmentInteractionListener,
+                                                    CalendarFragment.OnFragmentInteractionListener,
+                                                    MoreOptionsFragment.OnFragmentInteractionListener {
 
-    private FragmentActivity fragment;
+    private Fragment fragment;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragment = new FragmentActivity();
-
+        
+        fragmentManager = getSupportFragmentManager();
+        fragment = new HomeFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment).commit();
+        
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
@@ -33,14 +41,20 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         @StringRes int text;
         switch (item.getItemId()) {
             case R.id.menu_home:
+                fragment = new HomeFragment();
                 break;
             case R.id.menu_calendar:
+                fragment = new CalendarFragment();
                 break;
             case R.id.menu_more:
+                fragment = new MoreOptionsFragment();
                 break;
             default:
                 return false;
         }
+
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment).commit();
         return true;
     }
 
@@ -49,4 +63,8 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         startActivity(intent);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //
+    }
 }
